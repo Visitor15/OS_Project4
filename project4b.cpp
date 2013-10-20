d027db25cbd22340a01b916d856f2cb7bdcb6da5
visitor15 <visitor15@gmail.com>
2013-10-20 16:44:19 -0400

    Continued work on project 4 part b.

--- project4b.cpp
+++ project4b.cpp
@@ -1,116 +1,353 @@
-//  CS3242 Operating Systems
-//  Fall 2013
-//  Project 4: Process Synchronization, Part 1
-//  Nick Champagne and John Mutabazi
-//  Date: 10/23/2013
-//  File: project4a.cpp
-
-#include <cstdlib>
-#include <stdlib.h>
+/*
+ * project4b.cpp
+ *
+ *  Created on: Oct 19, 2013
+ *  Author: visitor15
+ */
+#include <stdio.h>
 #include <iostream>
-#include <pthread.h>
-#include <cstdio>
-#include "buffer.h"
-#include <semaphore.h>
-#include <signal.h>
-#include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <string.h>
+#include <semaphore.h>
 
-using namespace std;
+static const long ARRAY_SIZE = 10000;
 
-void *producer(void *item);
-void *consumer(void *arg);
+// Global variables
+int* num_of_running_processes = 0;
+int* curr_index = 0;
+long* p_master_unsorted_list;
+long* p_master_sorted_list;
+bool* should_sort = false;
 
+//Global semaphore
+sem_t* m_semaphore;
 
-sem_t full;
-//int rc =sem_init(&full, 1, 0);
-sem_t empty;
-//int s=sem_init(&empty, 1, 10);
-pthread_mutex_t mutex;
+// Arrays
+long master_unsorted_list[ARRAY_SIZE];
+long master_sorted_list[ARRAY_SIZE];
+int* child_processes;
 
-//int p=sem_init(&mutex, 1, 1);
+// Local process variables
+pid_t pid;
+int num_child_processes = 4;
+int working_list_size = 2500;
+int process_index;
 
+std::vector<long> working_list;
+std::vector<long> unsorted_list;
 
-int main(int argc, char *argv[])
-{
-    printf("main starting:");
-    int tts= *((int *)argv[1]); //time to sleep
-    
-    
-    //am cheating
-    //*((int *)vptr_value);
-    
-    pthread_t pid, pid1;
-    pthread_attr_t attr; //thread attributes
-    pthread_attr_init(&attr);
-    pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM);
-    
-    pthread_create(&pid, &attr, producer, NULL);
-    pthread_create(&pid1, &attr, consumer, NULL);
-    pthread_join(pid, NULL);
-    pthread_join(pid1, NULL);
-    
-    printf("producer process ", pid);
-    printf("consumer process ", pid1);
-    
-    
-    int numbOfPro= *((int*)argv[2]); //number of producers
-    int numbOfCon= *((int*)argv[3]);
-    
-    
-    buf_init(); //initializing buffer
-    
-    cout<< "Number of producer produced" << producer(&numbOfPro);
-    cout<< "Number of producer produced" << consumer(&numbOfCon);
-    usleep(tts);
-    exit(0);
+void readInData(std::vector<long> &list);
+void initSharedMemoryObjs();
+void spawnChildrenProcesses(int num_of_children);
+void doSelectionSort(std::vector<long> &unsortedList);
+std::vector<long> getWorkingList(long* list, int offset, int length);
+void logSpawnedChild(int pid);
+void doChildProcessWork();
+void onChildProcessFinished(std::vector<long> &unsortedList);
+void appendElementsToMasterList(std::vector<long> &list);
+void exitAndCleanChildProcess();
+void exitAndCleanParentProcess();
+void writeToScreen(std::vector<long> &list);
+void doMergeSort(long list[]);
+long *mergeUnsortedLists(long firstList[], long secondList[]);
+
+int main() {
+
+	child_processes = new int[num_child_processes];
+	readInData(unsorted_list);
+	initSharedMemoryObjs();
+	for (int i = 0; i < unsorted_list.size(); i++) {
+		p_master_unsorted_list[i] = unsorted_list.at(i);
+	}
+
+	spawnChildrenProcesses(num_child_processes);
+
+	while (*num_of_running_processes > 0) {
+		if (*should_sort) {
+//			sem_wait(m_semaphore);
+//			doMergeSort(p_master_unsorted_list);
+//
+//			for (int i = 0; i < ARRAY_SIZE; i++) {
+//				std::cout << p_master_sorted_list[i] << std::endl;
+//			}
+//			sem_post(m_semaphore);
+		} else {
+			sleep(1);
+		}
+	}
+
+	std::cout << "All processes finished!" << std::endl;
+
+	sem_wait(m_semaphore);
+	doMergeSort(p_master_unsorted_list);
+
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		std::cout << p_master_sorted_list[i] << std::endl;
+	}
+	sem_post(m_semaphore);
+
+	exitAndCleanParentProcess();
+
+	return 0;
 }
 
-void *producer(void *val)
-{
-    int count =0;
-    buffer_item item;
-    printf("Producer created");
-    do{
-        //will create threads here
-        item = rand();
-        sem_wait(&empty);
-        
-        //semaphore
-        
-        //sem_wait(&mutex);
-        pthread_mutex_lock(&mutex);
-        int result = insert_item(&item);
-        
-        printf((result==0?"successfully added":"failed to add"));
-        count++;
-        
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);     
-    }while(true);
-    return NULL;
+void readInData(std::vector<long> &list) {
+	std::ifstream inStream("numbers.txt");
+	std::string m_line;
+	while (std::getline(inStream, m_line)) {
+		//Converting from a string to an long.
+		long number = strtol((char*) m_line.c_str(), NULL, 0);
+
+		list.push_back(number);
+	}
+	inStream.close();
 }
 
-void *consumer(void *arg)
-{
-    int count =0;
-    printf("Consumer created");
-    do{
-        //will create threads here
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-        
-        int result =remove_item();
-        printf((result==0?"successfully removed":"failed to remove"));
-        count++;
-        
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-        count++;
-    }while(&count!=arg);
-    return NULL;
+void initSharedMemoryObjs() {
+
+	m_semaphore = (sem_t*) mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	sem_init(m_semaphore, 1, 1);
+
+	child_processes = (int*) mmap(NULL, sizeof(child_processes),
+	PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	num_of_running_processes = (int*) mmap(NULL,
+			sizeof(*num_of_running_processes),
+			PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	curr_index = (int*) mmap(NULL, sizeof(*curr_index), PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	p_master_unsorted_list = (long*) mmap(NULL, sizeof(long) * ARRAY_SIZE,
+	PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	p_master_sorted_list = (long*) mmap(NULL, sizeof(master_sorted_list),
+	PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	should_sort = (bool*) mmap(NULL, sizeof(bool), PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
 }
 
+void spawnChildrenProcesses(int num_of_children) {
 
+	for (int i = 1; i <= num_of_children; i++) {
+		pid = fork();
+
+		switch (pid) {
+		case -1: {
+			// Error has occurred
+
+			break;
+		}
+		case 0: {
+			// Child process
+
+			process_index = i;
+			logSpawnedChild(getpid());
+			doChildProcessWork();
+
+			break;
+		}
+		default: {
+			// Parent process
+
+			break;
+		}
+		}
+	}
+}
+
+void logSpawnedChild(int pid) {
+	sem_wait(m_semaphore);
+
+	child_processes[*num_of_running_processes] = getpid();
+	*num_of_running_processes = *num_of_running_processes + 1;
+
+	std::cout << "Child " << process_index << " running" << std::endl;
+
+	sem_post(m_semaphore);
+}
+
+void doChildProcessWork() {
+	int offset = ((process_index - 1) * working_list_size) - 1;
+	working_list = getWorkingList(p_master_unsorted_list, offset,
+			working_list_size);
+
+	doSelectionSort(working_list);
+
+//	sem_wait(m_semaphore);
+//	for (int i = 0; i < 10; i++) {
+//		std::cout << "PID: " << process_index << " " << working_list.at(i)
+//				<< std::endl;
+//	}
+//	sem_post(m_semaphore);
+
+	exitAndCleanChildProcess();
+}
+
+std::vector<long> getWorkingList(long* list, int offset, int length) {
+	std::vector<long> tmp_list;
+
+	for (int i = offset; i < (length + offset); i++) {
+		tmp_list.push_back(list[i]);
+	}
+
+	return tmp_list;
+}
+
+void writeToScreen(std::vector<long> &list) {
+	sem_wait(m_semaphore);
+
+	/*
+	 * CRITICAL SECTION
+	 */
+	std::cout << "PID: " << process_index << std::endl;
+	for (int i = 0; i < list.size(); i++) {
+		std::cout << list.at(i) << std::endl;
+	}
+
+	sem_post(m_semaphore);
+}
+
+void doSelectionSort(std::vector<long> &unsortedList) {
+	long minIndex;
+	long tempVal;
+	long i;
+	long j;
+
+	for (i = 0; i < unsortedList.size() - 1; i++) {
+		minIndex = i;
+
+		for (j = i + 1; j < unsortedList.size(); j++) {
+
+			if (unsortedList.at(j) < unsortedList.at(minIndex)) {
+				minIndex = j;
+			}
+		}
+
+		if (minIndex != i) {
+			tempVal = unsortedList.at(i);
+			unsortedList.at(i) = unsortedList.at(minIndex);
+			unsortedList.at(minIndex) = tempVal;
+		}
+	}
+
+	std::vector<long> sortedList = unsortedList;
+	onChildProcessFinished(sortedList);
+}
+
+void onChildProcessFinished(std::vector<long> &unsortedList) {
+
+	// Critical section
+	appendElementsToMasterList(unsortedList);
+}
+
+void appendElementsToMasterList(std::vector<long> &list) {
+	for (int i = 0; i < list.size(); i++) {
+
+		if (i & 10 == 0) {
+			sem_wait(m_semaphore);
+			p_master_sorted_list[*curr_index] = list.at(i);
+
+			// Incrementing the global index
+			*curr_index = *curr_index + 1;
+
+			// We appended to elements to the master sorted list.
+			// We need to notify the parent process it should sort this list again.
+			*should_sort = true;
+			sem_post(m_semaphore);
+		}
+	}
+}
+
+void doMergeSort(long list[]) {
+	int list_len = (sizeof(list) / sizeof(long));
+	int mMiddle = list_len / 2;
+	long mLeft[mMiddle];
+	long mRight[mMiddle];
+
+	if (list_len > 1) {
+		for (int i = 0; i < mMiddle; i++) {
+			mLeft[i] = list[i];
+		}
+
+		for (int j = mMiddle; j < list_len; j++) {
+			mRight[j] = list[j];
+		}
+
+		doMergeSort(mLeft);
+		doMergeSort(mRight);
+		p_master_sorted_list = mergeUnsortedLists(mLeft, mRight);
+	}
+}
+
+long *mergeUnsortedLists(long firstList[], long secondList[]) {
+	std::vector<long> finalSortedList;
+
+	int first_length = (sizeof(firstList) / sizeof(long));
+	int second_length = (sizeof(secondList) / sizeof(long));
+
+	long sorted_list[first_length + second_length];
+
+	int current_index = 0;
+	int tmp_index = 0;
+
+	while (first_length > current_index || second_length > current_index) {
+		if (first_length > current_index && second_length > current_index) {
+			if (firstList[current_index] <= secondList[current_index]) {
+				sorted_list[current_index] = firstList[tmp_index];
+
+			} else {
+				sorted_list[current_index] = secondList[tmp_index];
+			}
+			++current_index;
+		} else if (first_length > current_index) {
+			sorted_list[current_index] = firstList[tmp_index];
+		} else if (second_length > current_index) {
+			sorted_list[current_index] = secondList[tmp_index];
+		}
+
+		tmp_index = 0;
+	}
+
+	return sorted_list;
+}
+
+void exitAndCleanChildProcess() {
+	sem_wait(m_semaphore);
+
+	std::cout << "Child " << process_index << " is finishing" << std::endl;
+	*num_of_running_processes = *num_of_running_processes - 1;
+
+	sem_post(m_semaphore);
+
+	exit(EXIT_SUCCESS);
+}
+
+void exitAndCleanParentProcess() {
+	sem_destroy(m_semaphore);
+	munmap(m_semaphore, sizeof(sem_t));
+	munmap(p_master_unsorted_list,
+			sizeof(p_master_unsorted_list[0]) * ARRAY_SIZE);
+	munmap(p_master_unsorted_list,
+			sizeof(p_master_sorted_list[0]) * ARRAY_SIZE);
+	munmap(num_of_running_processes, sizeof(*num_of_running_processes));
+	munmap(child_processes, sizeof(child_processes));
+
+	exit(EXIT_SUCCESS);
+}
 
